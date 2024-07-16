2feb97ed80326128d70b96bfc9efcccc45d06c58
123-123-1 <152476783+123-123-1@users.noreply.github.com>
2024-07-16 18:20:07 +0800

    更新

--- user/trace.c
+++ user/trace.c
@@ -3,20 +3,25 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int argc ,char* argv[])
+int
+main(int argc, char *argv[])
 {
-    char * newargv[MAXARG];
-    if(argc<3){
-        fprintf(2,"TRACE: wrong command\n");
-        exit(1);
-    }
-    if(atoi(argv[1])<0){
-        fprintf(2,"TRACE: wrong mask\n");
-        exit(1);
-    }
-    for(int i=2;i<argc&&i<MAXARG;i++){
-        newargv[i-2]=argv[i];
-    }
-    exec(newargv[0],newargv);
-    exit(0);
+  int i;
+  char *nargv[MAXARG];
+
+  if(argc < 3 || (argv[1][0] < '0' || argv[1][0] > '9')){
+    fprintf(2, "Usage: %s mask command\n", argv[0]);
+    exit(1);
+  }
+
+  if (trace(atoi(argv[1])) < 0) {
+    fprintf(2, "%s: trace failed\n", argv[0]);
+    exit(1);
+  }
+  
+  for(i = 2; i < argc && i < MAXARG; i++){
+    nargv[i-2] = argv[i];
+  }
+  exec(nargv[0], nargv);
+  exit(0);
 }
