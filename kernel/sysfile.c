d8375772285201e04a6897d9dae6d378d62cdda1
123-123-1 <3182821816@qq.com>
2024-08-27 22:22:48 +0800

    finish

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -322,6 +322,34 @@ sys_open(void)
     return -1;
   }
 
+  if(ip->type==T_SYMLINK&&!(omode&O_NOFOLLOW)){
+    int depth=10;
+    char target[MAXPATH];
+    for(int i=0;i<depth;i++){
+      if(readi(ip,0,(uint64)target,0,MAXPATH)<MAXPATH){
+        iunlock(ip);
+        end_op();
+        return -1;
+      }
+      iunlock(ip);
+
+      if((ip=namei(target))==0){
+        end_op();
+        return -1;
+      }
+      
+      ilock(ip);
+      if(ip->type!=T_SYMLINK){
+        break;
+      }
+      if(i==depth-1){
+        iunlock(ip);
+        end_op();
+        return -1;
+      }
+    }
+  }
+
   if((f = filealloc()) == 0 || (fd = fdalloc(f)) < 0){
     if(f)
       fileclose(f);
@@ -484,3 +512,31 @@ sys_pipe(void)
   }
   return 0;
 }
+
+uint64 
+sys_symlink(void)
+{
+  char target[MAXPATH];
+  char path[MAXPATH];
+  struct inode* ip;
+
+  if(argstr(0,target,MAXPATH)<0||argstr(1,path,MAXPATH)<0){
+    return -1;
+  }
+  begin_op();
+  
+  ip=create(path,T_SYMLINK,0,0);
+  if(ip==0){
+    end_op();
+    return -1;
+  }
+  
+  if(writei(ip,0,(uint64)target,0,MAXPATH)<MAXPATH){
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
+  return 0;
+} 
